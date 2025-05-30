71061fdeca0f64c237241e87aae44b60da30ee89
phamt <phamthiennhan0204@gmail.com>
2025-05-30 16:05:39 +0000

    fix register

--- src/main/java/com/bookstore/controller/UsersServlet.java
+++ src/main/java/com/bookstore/controller/UsersServlet.java
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebServlet(name="/users")
+@WebServlet("/users")
 public class UsersServlet extends HttpServlet {
     
     UsersDAO usersDAO = new UsersDAO();
